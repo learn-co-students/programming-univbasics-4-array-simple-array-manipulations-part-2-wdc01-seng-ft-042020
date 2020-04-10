4f1198713bb77574b06c4385812a725d9153ec68
Andrew Sargeant <agdsargeant@gmail.com>
2020-04-10 19:58:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(arr1, arr2)
+  return arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  return array.insert(4, element)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  return array.delete_at(integer)
+end
